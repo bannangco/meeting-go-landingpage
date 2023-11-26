5a57b7d0cad916defaa5ff439c7e6686a6319169
Yunbo Shim <ybuser@naver.com>
2023-11-27 01:22:04 +0900

    add: Navbar 완료

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,63 +2,102 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 const NavContainer = styled.nav`
-  background-color: #fff; // Changed to white as per the image
+  background-color: #fff;
   display: flex;
-  justify-content: flex-end; // Align items to the right
+  justify-content: space-between;
   align-items: center;
-  height: 50px; // Set a fixed height for the navbar
-  padding: 0 20px; // Padding on the sides
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); // Subtle shadow as per the image
-  z-index: 10; // Ensure navbar is above other content
+  height: 50px;
+  padding: 0 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  z-index: 10;
+`;
+
+const Logo = styled.img`
+  max-height: 50px;
+  max-width: 100%; 
+  flex-shrink: 0;
+`;
+
+const LogoLink = styled(NavLink)`
+  display: flex; // Keep the logo aligned properly
+  align-items: center; // Center the logo vertically
+`;
+
+
+const NavBarLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const NavBarRight = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const NavBarMenus = styled.ul`
   display: flex;
-  justify-content: flex-end; // Align items to the right
   list-style: none;
-  padding-left: 0px;
-  margin: 0; // Remove default margin
+  margin: 0;
 `;
 
 const NavBarMenusMenu = styled.li`
-  padding: 0 10px; // Spacing between menu items
+  padding: 0 10px;
 `;
 
 const NavStyle = styled(NavLink)`
-  color: black; // Text color as per the image
+  color: black;
   text-decoration: none;
   &:hover {
-    color: #000; // Adjust the color for hover effect
-    text-decoration: none; // No underline on hover
+    color: #000;
+    text-decoration: none;
   }
   &.active {
-    color: #000; // Active link color
-    font-weight: bold; // Make active link bold
+    color: #000;
+    font-weight: bold;
+  }
+`;
+
+const SpecialButton = styled(NavLink)`
+  background-color: black;
+  color: white;
+  padding: 10px 15px;
+  border-radius: 5px; 
+  text-decoration: none;
+  margin-left: 10px; 
+  &:hover {
+    background-color: #333;
   }
 `;
 
 function NavBar() {
-    return (
-      <NavContainer>
+  return (
+    <NavContainer>
+      <NavBarLeft>
+        <LogoLink to="/">
+            <Logo src="/logo/logostransparent1.png" alt="Logo" />
+        </LogoLink>
+        {/* <NavBarMenus>
+          <NavBarMenusMenu>
+            <NavStyle to="/form">대학 미팅 찾기</NavStyle>
+          </NavBarMenusMenu>
+          <NavBarMenusMenu>
+            <NavStyle to="/form">동네 미팅 찾기</NavStyle>
+          </NavBarMenusMenu>
+        </NavBarMenus> */}
+      </NavBarLeft>
+      <NavBarRight>
         <NavBarMenus>
           <NavBarMenusMenu>
-            <NavStyle to="/tournament">대학 미팅 찾기</NavStyle>
+            <NavStyle to="/">Home</NavStyle>
           </NavBarMenusMenu>
           <NavBarMenusMenu>
-            <NavStyle to="/home">동네 미팅 찾기</NavStyle>
-          </NavBarMenusMenu>
-          <NavBarMenusMenu>
-            <NavStyle to="/start">시작하기</NavStyle>
-          </NavBarMenusMenu>
-          <NavBarMenusMenu>
-            <NavStyle to="/help">Help</NavStyle>
-          </NavBarMenusMenu>
-          <NavBarMenusMenu>
-            <NavStyle to="/login">Login</NavStyle>
+            <NavStyle to="/form">Login</NavStyle>
           </NavBarMenusMenu>
         </NavBarMenus>
-      </NavContainer>
-    );
-  }
+        <SpecialButton to="/form">시작하기</SpecialButton>
+      </NavBarRight>
+    </NavContainer>
+  );
+}
 
 export default NavBar;
