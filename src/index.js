47ef05541a446b9966685963a1f4a47d820426ee
Yunbo Shim <ybuser@naver.com>
2024-05-31 01:41:13 +0900

    add: react-snap, react-helmet 이용한 페이지별 seo

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { HelmetProvider } from "react-helmet-async";
+import { hydrate, render } from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>
-);
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </React.StrictMode>
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </React.StrictMode>
+  );
+}
